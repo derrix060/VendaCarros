8e768fa32a94f3070db979390ea0df829ab33631
derrix060 <mario_surfing@hotmail.com>
2016-05-11 22:25:50 -0300

    Add files via upload
    TODO: remodelar para o sistema MVC com DAO

---
+++ src/model/ClienteDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author aluno
+ */
+public class ClienteDAO {
+    Connection conexao;
+    DriverManager driver;
+    Statement stmt;
+    ResultSet rs;
+    
+    public void gravarCliente (Cliente cliente){
+        
+        //
+    }
+}
